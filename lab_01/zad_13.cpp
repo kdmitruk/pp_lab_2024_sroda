1e8e3291cd828ae87c3a74a568c2357bfc6f47aa
Krzysztof Dmitruk <kdmitruk@gmail.com>
2024-10-02 15:15:23 +0200

    zad 13 15

--- lab_01/zad_13.cpp
+++ lab_01/zad_13.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int tranzystory1 = 2250;
+    u_int64_t tranzystory2;
+    int rok1 = 1971;
+    int rok2 = 2023;
+
+    tranzystory2 = tranzystory1 * pow(2, (rok2-rok1)/2);
+
+
+
+    cout << tranzystory2 << endl;
     return 0;
 }
--- lab_01/zad_15.cpp
+++ lab_01/zad_15.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    long int tran1 = 2'250, tran2 = 5'000'000'000;
+    int rok1 = 1971, rok2 = 2012;
+    int mies = (rok2 - rok1)*12;
+    float x = mies/log2((float)tran2/tran1);
+    cout << x << endl;
     return 0;
 }
