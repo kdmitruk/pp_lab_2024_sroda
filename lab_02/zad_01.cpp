86f03d2a4e462f6a9f4935fd3ae448c8eaa6e5ca
Krzysztof Dmitruk <kdmitruk@gmail.com>
2024-10-09 12:16:55 +0200

    z1

--- lab_02/zad_01.cpp
+++ lab_02/zad_01.cpp
@@ -4,6 +4,17 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    double a,b;
+    cin >> a;
+    cin >> b;
+    cout << "a =" << a << endl;
+    cout << "b =" << b << endl;
+    double temp;
+    temp = a;
+    a = b;
+    b = temp;
+    cout << "a =" << a << endl;
+    cout << "b =" << b << endl;
+
     return 0;
 }
