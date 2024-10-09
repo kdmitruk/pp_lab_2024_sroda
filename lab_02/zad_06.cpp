379b6f3e56ae4585cde399883b7b1f3b6a8de78b
Krzysztof Dmitruk <kdmitruk@gmail.com>
2024-10-09 14:00:07 +0200

    z6

--- lab_02/zad_06.cpp
+++ lab_02/zad_06.cpp
@@ -4,6 +4,39 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    double a1,a2,b1,b2;
+    a1=2;
+    a2=4;
+    b1=1;
+    b2=3;
+    bool condition1 = b1<=a2;
+    bool condition2 = b2>=a1;
+    if(condition1 && condition2)
+    {
+        cout << "intersection" << endl;
+        if (a1>b1)
+        {
+            cout << a1 << endl;
+        }
+        else
+        {
+            cout << b1 << endl;
+        }
+
+        if(a2<b2)
+        {
+            cout << a2 << endl;
+        }
+        else
+        {
+            cout << b2 << endl;
+        }
+    }
+    else
+    {
+        cout << "no intersection" << endl;
+
+    }
+
     return 0;
 }
