b5df64784fbc08aecc0cdf1fd5e71db8fe6291a5
Krzysztof Dmitruk <kdmitruk@gmail.com>
2024-10-09 14:59:00 +0200

    z17

--- lab_02/zad_17.cpp
+++ lab_02/zad_17.cpp
@@ -4,6 +4,15 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int rok;
+    cin >> rok;
+    if (rok%4==0 && (rok%100!=0 || rok%400==0))
+    {
+        cout << "leap year" << endl;
+    }
+    else
+    {
+        cout << "common year" << endl;
+    }
     return 0;
 }
