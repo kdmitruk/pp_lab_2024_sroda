2c5561d980e0f3abfe9bd5cc70ea90c4ede613cf
Krzysztof Dmitruk <kdmitruk@gmail.com>
2024-10-16 12:56:58 +0200

    zad 4

--- lab_03/zad_04.cpp
+++ lab_03/zad_04.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
-
+#include <climits>
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int n, l, ml=INT_MIN;
+    cin >> n;
+    for (int i=0; i<n; i++)
+    {
+        cin >> l;
+        ml = ml<l ? l : ml;
+        /*if(ml<l)
+        {
+            ml=l;
+        }*/
+    }
+    cout << ml << endl;
     return 0;
 }
