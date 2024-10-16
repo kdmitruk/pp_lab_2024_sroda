f70989c5aac0c2f9e9165a7df401bb81039cbbb0
Krzysztof Dmitruk <kdmitruk@gmail.com>
2024-10-16 14:18:41 +0200

    zad 11

--- lab_03/zad_11.cpp
+++ lab_03/zad_11.cpp
@@ -4,6 +4,17 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int m, n;
+    cin >> m >> n;
+    for (int i=0; i<m; i++)
+    {
+        for (int j=0; j<n; j++)
+        {
+            char s = i%2==0 ? (j%2==0 ? '#' : '0') : (j%2==0 ? '0' : '#');
+            cout << s;
+        }
+        cout << endl;
+    }
+
     return 0;
 }
