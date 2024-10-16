77bdf5ed23356e5649cb11815bd9603a0096e195
Krzysztof Dmitruk <kdmitruk@gmail.com>
2024-10-16 13:07:41 +0200

    zad 6

--- lab_03/zad_06.cpp
+++ lab_03/zad_06.cpp
@@ -4,6 +4,14 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    float x;
+    cout << "podaj liczby " << endl;
+    float sum=0;
+    do{
+        cin >> x;
+        sum += x;
+    }
+    while (sum < 100);
+    cout << sum-100;
     return 0;
 }
