42433c82d8eb627fd92b25c5dc13a4fc94e76d3b
Krzysztof Dmitruk <kdmitruk@gmail.com>
2024-10-16 14:39:59 +0200

    zadania 11 i 12

--- lab_03/zad_12.cpp
+++ lab_03/zad_12.cpp
@@ -4,6 +4,15 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    int x=10;
+    int count=0;
+    if (x==0)
+        count=1;
+    /*else while(x != 0)
+    {
+        x=x/10;
+        count++;
+    }*/
+    for( ; x != 0;x/=10,count++);
+    cout<<count<<endl;
 }
