60bd4eda692caa1c4db6713971f77aac211bad3b
Krzysztof Dmitruk <kdmitruk@gmail.com>
2024-10-16 13:34:04 +0200

    Zadanie 9

--- lab_03/zad_09.cpp
+++ lab_03/zad_09.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
+#include <cstdio>
+#include <iomanip>
 
 using namespace std;
 
+/*int main()
+{
+    for(int y = 0; y <= 10; y++){
+        for(int x = 0; x <=y; x++){
+            printf("%4d", x*y);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+*/
 int main()
 {
-    cout << "Hello World!" << endl;
+    for(int y = 0; y <= 10; y++){
+        for(int x = 0; x < y; x++) //spacje
+            printf("    ");
+        for(int x = y; x <= 10; x++){ //dane
+            printf("%4d", x*y);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
