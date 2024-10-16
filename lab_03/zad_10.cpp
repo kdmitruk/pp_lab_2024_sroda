48738f5bd5267d94044de422df3209cd8a9f9c7b
Krzysztof Dmitruk <kdmitruk@gmail.com>
2024-10-16 14:04:52 +0200

    Zadanie 10

--- lab_03/zad_10.cpp
+++ lab_03/zad_10.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 
 using namespace std;
-
 int main()
 {
-    cout << "Hello World!" << endl;
+    int n=10;
+    for(int y = 0; y < n; y++){
+        int space_count=n-y-1;
+       for(int x = 0; x < space_count; x++)
+            printf(" ");
+        int star_count= y*2+1;
+        for(int x = 0; x<star_count; x++){
+            printf("*");
+        }
+        printf("\n");
+    }
+
     return 0;
 }
