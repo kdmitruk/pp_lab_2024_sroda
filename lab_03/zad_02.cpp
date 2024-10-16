1397d26588b8593283174763158429db1047ef6d
Krzysztof Dmitruk <kdmitruk@gmail.com>
2024-10-16 12:38:05 +0200

    zad 3

--- lab_03/zad_02.cpp
+++ lab_03/zad_02.cpp
@@ -8,14 +8,14 @@ int main()
     cout << "podaj liczbe calkowita" << endl;
     cin >> x;
     if(x>0){
-    for(int i=0; i <= x; i++){
-        cout << i << " ";
+        for(int i=0; i <= x; i++){
+            cout << i << " ";
+        }
     }
-           }
     if(x<0){
         for(int i=x; i <= 0; i++){
             cout << i << " ";
-            }
+        }
     }
     return 0;
 }
--- lab_03/zad_03.cpp
+++ lab_03/zad_03.cpp
@@ -4,6 +4,15 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int n;
+    cin >> n;
+    int m;
+    int suma = 0;
+    for(int i = 0; i < n; i++){
+        cin >> m;
+        suma = suma + m;
+    }
+
+    cout << (float)suma/n << endl;
     return 0;
 }
