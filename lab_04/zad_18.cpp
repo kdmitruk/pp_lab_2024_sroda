c9f58daa1248f3bb42828b51c02c8afa43609ca2
Krzysztof Dmitruk <kdmitruk@gmail.com>
2024-10-30 12:41:32 +0100

    18 19

--- lab_04/zad_18.cpp
+++ lab_04/zad_18.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
-
+#include <iomanip>
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    const int size = 16;
+    int macierz[size][size];
+    for(int i = 0; i<size; i++){
+        for(int j = 0; j<size;j++){
+            macierz[i][j] = (i+1)*(j+1);
+        }
+    }
+    for(int i = 0; i<size; i++){
+        for(int j = 0; j<size;j++){
+            cout << setw(3)<<macierz[i][j] << " ";
+
+        }
+        cout<<endl;
+    }
     return 0;
 }
