b3cd49a3427f3113caa27cb8f40a53db37735580
Krzysztof Dmitruk <kdmitruk@gmail.com>
2024-11-13 15:32:24 +0100

    zad 6 + poprawione 4

--- lab_07/zad_04.cpp
+++ lab_07/zad_04.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-int binToInt(char bin[], int index)
-{
-    if(index == -1)
-        return 0;
-    else
-    {
-        return binToInt(bin, index-1)*10+(bin[index]-'0')%10;
-    }
+int binToInt(char bin[], int index, int size) {
+    if(index == size)
+        return bin[index] - '0';
+
+    int rest = binToInt(bin, index + 1, size);
+    int digit = bin[index] - '0';
+
+    return digit * pow(2, size - index) + rest;
 }
 
-int main()
-{
-    char bin[] = "10010011";
-    //int size = sizeof(bin)-1;
-    cout << binToInt(bin, 8);
+int main() {
+    char bin[] = "1101001";
+    int size = sizeof(bin) / sizeof(bin[0]) - 2;
+    cout << binToInt(bin, 0, size) << endl;
     return 0;
 }
--- lab_07/zad_06.cpp
+++ lab_07/zad_06.cpp
@@ -1,10 +1,31 @@
 #include <iostream>
-bool isPowerOfTwo(unsigned int x)
-{
-    return !(x & (x-1));
-}
+#include <iomanip>
 
+using namespace std;
+
+void showTable(int macierz[][3], int rows, int cols)
+{
+    for (int i=0; i<rows; i++)
+    {
+        for (int j=0; j<cols; j++)
+        {
+            cout << setw(4) << macierz[i][j];
+        }
+        cout << endl;
+    }
+}
+void dyadic(int arr1[], int arr2[], int tab[], int n){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            tab[i*n+j]= arr1[i]*arr2[j];
+        }
+    }
+}
 int main() {
-    int x = 1024;
-    std::cout << isPowerOfTwo(x) << std::endl;
+    int arr1[] = {1, 2, 3};
+    int arr2[] = {10, 20, 30};
+    int tab[3][3];
+    dyadic(arr1, arr2, (int*)tab, 3);
+    showTable(tab, 3, 3);
+    return 0;
 }
