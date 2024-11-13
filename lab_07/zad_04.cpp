d1180a1f564bc70d0448f480012783f64acff724
Krzysztof Dmitruk <kdmitruk@gmail.com>
2024-11-13 14:50:32 +0100

    zad 4 i zad 5

--- lab_07/zad_04.cpp
+++ lab_07/zad_04.cpp
@@ -1,53 +1,21 @@
 #include <iostream>
+
 using namespace std;
-int bit_set_1(int x, int index){
-    int mask = 1 << index;
 
-    return (x | mask);
-}
-
-int bit_set_0(int x, int index){
-    int mask = 1 << index;
-
-    return (x & ~mask);
-}
-int bit_reverse(int x, int index){
-    int mask = 1 << index;
-
-    return (x & mask ) ? bit_set_0(x, index): bit_set_1(x, index);
-}
-void print_bin(int x){
-    // unsigned int x = 1024;//INT_MAX +1;
-    unsigned int index = 1<<31;
-
-    /*while(x>0){
-
-        cout <<(x & index)<<endl;
-        x >>= 1;
-    }*/
-    bool print = false;
-    while(index != 0){
-        int digit =(x & index)!=0 ? 1:0;
-        if(!print){
-            if (digit==1)
-                print = true;
-        }
-        if(print)
-            cout <<((x & index)!=0 ? 1:0);
-
-        index >>= 1;
+int binToInt(char bin[], int index)
+{
+    if(index == -1)
+        return 0;
+    else
+    {
+        return binToInt(bin, index-1)*10+(bin[index]-'0')%10;
     }
 }
 
-
-
 int main()
 {
-    int x = 1024;
-    int index = 2;
-    print_bin(bit_reverse(x, index));
-    // print_bin(bit_set_1(x, index));
-    // cout<<bit_set_0(x, index)<<endl;
-    // print_bin(bit_set_0(x, index));
+    char bin[] = "10010011";
+    //int size = sizeof(bin)-1;
+    cout << binToInt(bin, 8);
     return 0;
 }
--- lab_07/zad_05.cpp
+++ lab_07/zad_05.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 
-#include <iostream>
-bool isEven(int number){
-    return !(number & 1);
+float changeArray(float arr[], int n)
+{
+    if(n == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return pow(-1, n) * arr[n-1] + changeArray(arr, n-1);
+    }
+
 }
+
 int main() {
-    std::cout<<isEven(1)<<std::endl;
+    int n;
+    cin >> n;
+    float arr[20];
+    for (int i=0; i<n; i++)
+    {
+        cin >> arr[i];
+    }
+    for(int i=1; i<=n; i++)
+    {
+        cout << changeArray(arr, i) << ", ";
+    }
     return 0;
 }
 
