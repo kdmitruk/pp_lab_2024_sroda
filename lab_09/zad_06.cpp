55013fb10ced95ae753ab1fecac3a517ac2c3635
Krzysztof Dmitruk <kdmitruk@gmail.com>
2024-11-27 14:52:10 +0100

    zad 6 7 8

--- lab_09/zad_06.cpp
+++ lab_09/zad_06.cpp
@@ -1,28 +1,33 @@
 #include <iostream>
-#include <iomanip>
-#define uchar unsigned char
+#include<cstdlib>
 
-
-using namespace std;
-
-void zad6(uint word, uchar *b0, uchar *b1, uchar *b2, uchar *b3)
-{
-    uint *pWord = &word;
-    uchar *pWordAsChar = (uchar *) pWord;
-    *b0 = pWordAsChar[0];
-    *b1 = pWordAsChar[1];
-    *b2 = pWordAsChar[2];
-    *b3 = pWordAsChar[3];
+void rand_gen(float *arr, int n){
+    float tmp=(float)RAND_MAX;
+    for(int i=0;i<n;i++)
+        arr[i]=rand()/tmp;
 }
+void printElements(float *arr1, int n1, int *arr2, int n2){
 
-int main() {
-    uint word = 0x41424344;
-    uchar b0;
-    uchar b1;
-    uchar b2;
-    uchar b3;
-    zad6(word, &b0, &b1, &b2, &b3);
-    //cout << (int)b0 << " " << (int)b1 << " " << (int)b2 << " " << (int)b3 << endl;
-    cout << b0 << " " << b1 << " " << b2 << " " << b3 << endl;
+    for(int i=0;i<n2;i++){
+        if(arr2[i]>=0 && arr2[i]<n1)
+            std::cout<<arr1[arr2[i]]<<' ';
+        else
+            std::cout<<"NAN"<<' ';
+    }
+}
+int main(){
+    int n1=10;
+    float *arr1=new float[n1];
+    rand_gen(arr1,n1);
+    for(int i=0;i<n1;i++)
+        std::cout<<arr1[i]<<' ';
+    std::cout<<'\n';
+    int n2=5;
+    int *arr2=new int[n2];
+    for(int i=0;i<n2;i++)
+        std::cin>>arr2[i];
+    printElements(arr1,n1, arr2, n2);
+    delete [] arr1;
+    delete [] arr2;
     return 0;
 }
--- lab_09/zad_07.cpp
+++ lab_09/zad_07.cpp
@@ -2,7 +2,27 @@
 
 using namespace std;
 
+void swap(void* a, void* b, int size )
+{
+    char temp;
+    char* ca = (char*)a;
+    char* cb = (char*)b;
+    for(int i = 0; i<size; i++)
+    {
+        temp = ca[i];
+        ca[i] = cb[i];
+        cb[i] = temp;
+    }
+}
+
 int main() {
-    cout << "Hello World!" << endl;
+    int a[3] { 6,2,4};
+    int b[3] {1,7,2};
+
+    swap(&a,&b,sizeof(a));
+    for(int i = 0; i<3; i++){
+        cout<<a[i]<<' '<<b[i]<<endl;
+    }
     return 0;
 }
+
