97b65fa22242904c547fc207eda0d124087aea54
Krzysztof Dmitruk <kdmitruk@gmail.com>
2025-01-15 13:09:45 +0100

    z3

--- lab_13/zad_03.cpp
+++ lab_13/zad_03.cpp
@@ -1,35 +1,23 @@
 #include <iostream>
-#include<cstring>
-#include<algorithm>
+
+
 using namespace std;
-bool condition(int x){
-    string xs=to_string(x);
-    int size=xs.length();
-    for(int i=0;i<size;i++){
-        char d=xs[i];
-        if(count_if(xs.begin(),xs.end(),[d](char c){return c==d;})>1)
-            return true;
-    }
-    return false;
-}
 
-bool condition2(int x){
-    return x%2 == 0;
-}
-void action (int &x){
-    string xs=to_string(x);
-    reverse(xs.begin(), xs.end());
-    x=stoi(xs);
+struct Point{
+    float x;
+    float y;
+};
 
-}
-void modify_array(int arr[], int n, bool (*c)(int), void (*a) (int &)){
+void copy(Point *source, Point destination[], int n){
     for(int i=0;i<n;i++)
-        if(c(arr[i]))
-            a(arr[i]);
+        destination[i]=source[i];
 }
+
 int main(){
-    int arr[10]={123,1233,1221,11233,456,12346,1001,100001,101,99001};
-    modify_array(arr,10,condition2,action);
-    for_each(arr,arr+10, [](int n){cout<<n<<' ';});
+    Point points1[3]={Point{2,2}, Point{1,2}, Point{2,1}};
+    Point points2[3];
+    copy(points1, points2, 3);
+    for(int i=0;i<3;i++)
+        cout<<points2[i].x<<"   "<<points2[i].y<<endl;
     return 0;
 }
