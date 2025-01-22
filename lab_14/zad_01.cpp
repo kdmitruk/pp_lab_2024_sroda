8661ce495feee032ae26b13c1daec9f5b7f4e507
Krzysztof Dmitruk <kdmitruk@gmail.com>
2025-01-22 14:43:43 +0100

    z1 skończone

--- lab_14/zad_01.cpp
+++ lab_14/zad_01.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <cstdio>
 #include <cstring>
+#include <algorithm>
 
 using namespace std;
 
 #include <stdio.h>      /* printf */
 #include <stdlib.h>     /* strtol */
+
+
 
 struct Player{
 
@@ -13,17 +16,22 @@ struct Player{
     unsigned int wins, games, frags;
 };
 
+struct PlayerPoints{
+    const Player* player;
+    unsigned int points;
+};
+
 Player createPlayer(const char *input){
     Player result;
     const char *endname;
     endname = strchr(input, ';');
-    cout << endname - input << endl;
+
     strncpy(result.name, input, endname - input);
-    cout<<result.name;
+
     int size = strlen(endname);
     char* temp = new char[size];
     memcpy(temp, endname, size);
-    cout << endl;
+
     char* semicolnPos = temp-1;
     while((semicolnPos = strchr(semicolnPos+1, ';')))
     {
@@ -31,16 +39,53 @@ Player createPlayer(const char *input){
         *semicolnPos = ' ';
 
     }
-    cout << temp;
+
+
+    result.wins = strtol(temp, &semicolnPos, 10);
+    result.games = strtol(semicolnPos, &semicolnPos, 10);
+    result.frags = strtol(semicolnPos, nullptr, 10);
+
+
+
+
+
 
 
     delete[] temp;
     return result;
 }
 
+bool compare(const PlayerPoints &a, const PlayerPoints &b)
+{
+    return a.points > b.points;
+}
+
+void ranking(const Player players[], int size)
+{
+   PlayerPoints* player_indices = new PlayerPoints[size];
+    for(int i=0; i<size; i++)
+   {
+       player_indices[i] = PlayerPoints{&players[i], players[i].frags + players[i].games + players[i].wins};
+       // cout << player_indices[i].player->name << endl;
+   }
+    sort(player_indices, player_indices + size, compare);
+
+   for(int i=0; i<size; i++)
+    {
+        cout << player_indices[i].player->name << " " << player_indices[i].points << endl;
+   }
+    delete[] player_indices;
+}
+
 int main ()
 {
-    createPlayer("MystiqueHero;1365;6890;15210");
+    Player players[4] = {
+        createPlayer("MystiqueHero;1365;6890;15210"),
+        createPlayer("Frizz;5400;6200;11000"),
+        createPlayer("Ziemniak;9965;11000;73000"),
+        createPlayer("Evel00na;10;10;90")
+    };
+    ranking(players, 4);
     // char szNumbers[] = "MystiqueHero;1365;6890;15210";
     // char * pEnd;
     // long int li1, li2, li3, li4;
